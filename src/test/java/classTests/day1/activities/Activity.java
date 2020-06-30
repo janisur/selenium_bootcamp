1ffb46a7a0d1d8bbdc2d342d8fb572b7de4b7eed
janisur <janis.urbasts@gmail.com>
2020-06-30 12:16:46 +0300

    COMMITTEST

--- src/test/java/classTests/day1/activities/Activity.java
+++ src/test/java/classTests/day1/activities/Activity.java
@@ -3,18 +3,29 @@ package classTests.day1.activities;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Activity {
 
+    private WebDriver driver;
+
     @Before
     public void setDriver() {
+        String userdirLib = System.getProperty("user.dir") + "\\lib\\";
+        System.setProperty("webdriver.chrome.driver", userdirLib + "chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
     @Test
     public void driverTest(){
+        driver.get("https://www.google.com/");
+        System.out.println(driver.getCurrentUrl());
     }
 
     @After
     public void closeBrowser() {
+        driver.quit();
     }
 }
